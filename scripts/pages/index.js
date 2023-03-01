b0fb977f8bac02d86ade7bb0f6043dd44efadbc8
Tom <thomasbalsack@gmail.com>
2023-03-01 15:02:08 +0100

    initial commit

---
+++ scripts/pages/index.js
@@ -0,0 +1,25 @@
+import data from '../data/recipes.js'
+// import SearchForm from '../template/SearchForm.js'
+import RecipeCard from '../templates/recipes-card.js'
+
+class IndexPage {
+    displayIndexData(recipes) {
+        if (recipes) {
+            // Generate Recipes Cards
+            recipes.forEach((recipe) => {
+                const card = new RecipeCard(recipe)
+                document.querySelector('#gallery').appendChild(card.render())
+            })
+        }
+    }
+
+    init() {
+        // Display Search & Tags Sections
+        document.querySelector('#searchSection').prepend(new SearchForm().display())
+        // Display All Recipes
+        this.displayIndexData(data)
+    }
+}
+
+const page = new IndexPage()
+page.init()
