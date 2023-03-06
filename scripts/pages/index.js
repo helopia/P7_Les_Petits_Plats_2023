c37ea8527fe937c998195b679adb7e1fe5b726e1
Tom <thomasbalsack@gmail.com>
2023-03-06 14:35:19 +0100

    First Algo + finitions weekend

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,24 +1,24 @@
 import data from '../data/recipes.js'
-import SearchForm from '../templates/search-form.js'
-import RecipeCard from '../templates/recipes-card.js'
+import SearchForm from '../template/SearchForm.js'
+import RecipeCard from '../template/RecipeCard.js'
 
 class IndexPage {
-    displayIndexData(recipes) {
-        if (recipes) {
-            // Generate Recipes Cards
-            recipes.forEach((recipe) => {
-                const card = new RecipeCard(recipe)
-                document.querySelector('#gallery').appendChild(card.render())
-            })
-        }
+  displayIndexData(recipes) {
+    if (recipes) {
+      // Generate Recipes Cards
+      recipes.forEach((recipe) => {
+        const card = new RecipeCard(recipe)
+        document.querySelector('#gallery').appendChild(card.render())
+      })
     }
+  }
 
-    init() {
-        // Display Search & Tags Sections
-        document.querySelector('#searchSection').prepend(new SearchForm().display())
-        // Display All Recipes
-        this.displayIndexData(data)
-    }
+  init() {
+    // Display Search & Tags Sections
+    document.querySelector('#searchSection').prepend(new SearchForm().display())
+    // Display All Recipes
+    this.displayIndexData(data)
+  }
 }
 
 const page = new IndexPage()
